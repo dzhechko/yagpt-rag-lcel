47e4ebba970bdce4c769c934af7deb9cecf12ce7
Dmitry Zhechkov <dzhechkov@yandex-team.ru>
2024-04-06 23:23:17 +0300

    2nd commit

--- YaGPT-RAG-bot.py
+++ YaGPT-RAG-bot.py
@@ -9,8 +9,6 @@ from yandex_chain import YandexLLM
 
 
 from langchain.prompts import PromptTemplate
-from langchain.document_loaders import DirectoryLoader, PyPDFLoader
-from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.vectorstores import OpenSearchVectorSearch
 
 from langchain.chains import RetrievalQA
@@ -25,11 +23,9 @@ from langchain_core.messages import AIMessage, HumanMessage, get_buffer_string
 from langchain_core.output_parsers import StrOutputParser
 
 from streamlit_chat import message
+from constants import MDB_OS_CA
 
 # from dotenv import load_dotenv
-
-ROOT_DIRECTORY = "."
-MDB_OS_CA = f"{ROOT_DIRECTORY}/.opensearch/root.crt"
 
 # использовать системные переменные из облака streamlit (secrets)
 # yagpt_api_key = st.secrets["yagpt_api_key"]
@@ -40,64 +36,6 @@ mdb_os_hosts = st.secrets["mdb_os_hosts"].split(",")
 mdb_os_index_name = st.secrets["mdb_os_index_name"]
 
 # MDB_OS_CA = st.secrets["mdb_os_ca"] # 
-
-def ingest_docs(temp_dir: str = tempfile.gettempdir()):
-    """
-    Инъекция ваших pdf файлов в MBD Opensearch
-    """
-    try:
-        # выдать ошибку, если каких-то переменных не хватает
-        if not yagpt_api_key or not yagpt_folder_id or not mdb_os_pwd or not mdb_os_hosts or not mdb_os_index_name:
-            raise ValueError(
-                "Пожалуйста укажите необходимый набор переменных окружения")
-
-        # загрузить PDF файлы из временной директории
-        loader = DirectoryLoader(
-            temp_dir, glob="**/*.pdf", loader_cls=PyPDFLoader, recursive=True
-        )
-        documents = loader.load()
-
-        # разбиваем документы на блоки
-        text_splitter = RecursiveCharacterTextSplitter(
-            chunk_size=chunk_size, chunk_overlap=chunk_overlap)
-        documents = text_splitter.split_documents(documents)
-        print(len(documents))
-
-        # подключаемся к базе данных MDB Opensearch, используя наши ключи (проверка подключения)
-        # conn = OpenSearch(
-        #     mdb_os_hosts,
-        #     http_auth=('admin', mdb_os_pwd),
-        #     use_ssl=True,
-        #     verify_certs=False,
-        #     ca_certs=MDB_OS_CA)
-        # для включения проверки MDB сертификата используйте verify_certs=True, также надо будет загрузить сертификат используя инструкцию по ссылке 
-        # https://cloud.yandex.ru/docs/managed-opensearch/operations/connect 
-        # и положить его в папку .opensearch/root.crt
-        
-        # инициируем процедуру превращения блоков текста в Embeddings через YaGPT Embeddings API, используя API ключ доступа
-        embeddings = YandexEmbeddings(folder_id=yagpt_folder_id, api_key=yagpt_api_key)
-        embeddings.sleep_interval = 0.1 #текущее ограничение эмбеддера 10 RPS, делаем задержку 1/10 секунды, чтобы не выйти за это ограничение
-        text_to_print = f"Ориентировочное время оцифровки = {len(documents)*embeddings.sleep_interval} с."
-        st.text(text_to_print)
-
-        # добавляем "документы" (embeddings) в векторную базу данных Opensearch
-        OpenSearchVectorSearch.from_documents(
-            documents,
-            embeddings,
-            opensearch_url=mdb_os_hosts,
-            http_auth=("admin", mdb_os_pwd),
-            use_ssl = True,
-            verify_certs = False,
-            ca_certs = MDB_OS_CA,
-            engine = 'lucene',
-            index_name = mdb_os_index_name,
-            bulk_size=1000000
-        )
-    # bulk_size - это максимальное количество embeddings, которое можно будет поместить в индекс
-
-    except Exception as e:
-        st.error(f"Возникла ошибка при добавлении ваших файлов: {str(e)}")
-
 
 # это основная функция, которая запускает приложение streamlit
 def main():
@@ -174,7 +112,7 @@ def main():
     # mdb_os_pwd = st.sidebar.text_input("MDB_OpenSearch_PASSWORD", type='password')
     # mdb_os_hosts = st.sidebar.text_input("MDB_OpenSearch_HOSTS через 'запятую' ", type='password').split(",")
     mdb_os_index_name = st.sidebar.text_input("MDB_OpenSearch_INDEX_NAME", type='password', value=mdb_os_index_name)
-    mdb_os_index_name = f"cba-{mdb_os_index_name}"
+    mdb_os_index_name = f"lcel-{mdb_os_index_name}"
 
     # yagpt_temp = st.sidebar.text_input("Температура", type='password', value=0.01)
     rag_k = st.sidebar.text_input("Количество поисковых выдач размером с один блок", type='password', value=5)
